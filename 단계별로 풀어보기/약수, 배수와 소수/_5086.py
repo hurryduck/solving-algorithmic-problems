039f3243b741841049236a663950200ab8230351
hurryduck <tigeryck0812@gmail.com>
2024-03-20 10:52:16 +0900

    Feat: 약수, 배수와 소수 1문제 추가
    1. 5086번: 배수와 약수

---
+++ 단계별로 풀어보기/약수, 배수와 소수/_5086.py
@@ -0,0 +1,14 @@
+# 배수와 약수
+while True:
+    num1, num2 = map(int, input().split())
+    # 0이 2개 주어질 경우 종료
+    if (num1 == num2):
+        break
+    # 약수
+    elif num2 % num1 == 0:
+        print("factor")
+    # 배수
+    elif num1 % num2 == 0:
+        print("multiple")
+    # 약수 배수 둘다 아님
+    print("neither")
