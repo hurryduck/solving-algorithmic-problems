9f996e48a05d4c862be24de6218f1ea343c17f83
hurryduck <tigeryck0812@gmail.com>
2024-03-23 12:02:23 +0900

    Feat: 1978번: 소수 찾기

---
+++ 단계별로 풀어보기/약수, 배수와 소수/_1978.py
@@ -0,0 +1,21 @@
+# 소수 찾기
+_ = input()
+n_list = list(map(int, input().split()))
+
+minority_num = 0
+for n in n_list:
+    is_minority = True
+    # 약수 확인
+    for div in range(2, n // 2 + 1):
+        if (n % div == 0):
+            is_minority = False
+            break
+    # 약수가 없다면 소수 개수 증가
+    if (is_minority):
+        minority_num += 1
+
+# 소수에서 1은 제외
+if 1 in n_list:
+    minority_num -= 1
+
+print(minority_num)
