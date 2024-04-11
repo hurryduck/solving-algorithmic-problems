7e7a484d7b3fdc3bdfe9a9416bf3b55f6d322b5f
hurryduck <tigeryck0812@gmail.com>
2024-04-11 09:34:16 +0900

    Feat: 10989

---
+++ BaekjoonOnlineJudge/java/src/_10989.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class _10989 {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.valueOf(br.readLine());
+        
+        int[] nums = new int[N];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Integer.valueOf(br.readLine());
+        }
+
+        Arrays.sort(nums);
+        for (int num : nums) {
+            sb.append(String.valueOf(num)).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) throws Exception{
+        new _10989().solution();
+    }
+}
