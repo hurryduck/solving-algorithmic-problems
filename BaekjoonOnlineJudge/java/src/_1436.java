3d425ea7e949883226d41c22e1f8b4e23683dcdd
hurryduck <tigeryck0812@gmail.com>
2024-04-12 16:38:04 +0900

    Feat: 1436

---
+++ BaekjoonOnlineJudge/java/src/_1436.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+
+public class _1436 {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.valueOf(br.readLine());
+        
+        int[] numArr = IntStream.rangeClosed(666, 2700000).toArray();
+        int[] endNumArr = Arrays.stream(numArr)
+            .filter(num -> String.valueOf(num).contains("666"))
+            .toArray();
+        
+        System.out.println(endNumArr[n - 1]);
+    }
+    
+    public static void main(String[] args) throws Exception{
+        new _1436().solution();
+    }
+}
