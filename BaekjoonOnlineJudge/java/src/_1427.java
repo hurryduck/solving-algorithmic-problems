7ed8f457b4937430ade606f93b8af4e6b393c83d
hurryduck <tigeryck0812@gmail.com>
2024-04-12 10:11:55 +0900

    Feat: 1427

---
+++ BaekjoonOnlineJudge/java/src/_1427.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;;
+
+public class _1427 {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String n = br.readLine();
+
+        Character[] charArr = new Character[n.length()];
+        for (int i = 0; i < charArr.length; i++) {
+            charArr[i] = n.charAt(i);
+        }
+
+        Arrays.sort(charArr, Collections.reverseOrder());
+
+        for (char aChar : charArr) {
+            bw.write(String.valueOf(aChar));
+        }
+        bw.flush();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new _1427().solution();
+    }
+}
