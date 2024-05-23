92357c6f6766c34bcfd22c8b2d57ec07ec51ff20
hurryduck <tigeryck0812@gmail.com>
2024-05-24 01:16:28 +0900

    Feat: 1009

--- BaekjoonOnlineJudge/java/src/rank/bronze/ii/_1009.java
+++ BaekjoonOnlineJudge/java/src/rank/bronze/ii/_1009.java
@@ -2,6 +2,8 @@ package rank.bronze.ii;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class _1009 {
     public static void main(String[] args) throws Exception {
@@ -15,10 +17,21 @@ public class _1009 {
         int t = Integer.valueOf(br.readLine()); // 테스트 케이스 t
         for (int i = 0; i < t; i++) {
             String[] inputStrSplit = br.readLine().split(" ");
-            int num = Integer.valueOf(inputStrSplit[0]); // 숫자
+            int num = Integer.valueOf(inputStrSplit[0]) % 10; // 숫자 (1의 자리만 필요)
             int exponent = Integer.valueOf(inputStrSplit[1]); // 지수
 
-            
+            List<Integer> remainderList = new ArrayList<>(); // 1의 자리의 리스트
+            int currentRemainder = num;
+            while (!remainderList.contains(currentRemainder)) {
+                remainderList.add(currentRemainder);
+                currentRemainder = (currentRemainder * num) % 10;
+            }
+
+            // The sequence of last digits is now in remainderList and it repeats after remainderList.size() elements
+            int cycleSize = remainderList.size();
+            int index = (exponent - 1) % cycleSize;
+            sb.append(remainderList.get(index) == 0 ? 10 : remainderList.get(index)).append("\n");
         }
+        System.out.println(sb);
     }
 }
